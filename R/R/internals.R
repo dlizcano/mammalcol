4a5d1d542f7a450734577ecaadce671c40a94b30
Baltazar Gonzalez <baltazargch@gmail.com>
2024-04-01 12:37:59 -0300

    Added internal function fix_departaments to fix names; some indentations changes

--- R/R/internals.R
+++ R/R/internals.R
@@ -5,28 +5,37 @@ standardize_names <- function(splist) {
   fixed2 <- gsub("cf\\.", "", fixed1)
   fixed3 <- gsub("aff\\.", "", fixed2)
   fixed4 <- trimws(fixed3) # remove trailing and leading space
-  fixed5 <- gsub("_", " ", fixed4) # change names separated by _ to space
-
+  fixed5 <-
+    gsub("_", " ", fixed4) # change names separated by _ to space
+  
   # Hybrids
   fixed6 <- gsub("(^x )|( x$)|( x )", " ", fixed5)
   hybrids <- fixed5 == fixed6
   if (!all(hybrids)) {
     sp_hybrids <- splist[!hybrids]
-    warning(paste("The 'x' sign indicating hybrids have been removed in the",
-                  "following names before search:",
-                  paste(paste0("'", sp_hybrids, "'"), collapse = ", ")),
-            immediate. = TRUE, call. = FALSE)
+    warning(
+      paste(
+        "The 'x' sign indicating hybrids have been removed in the",
+        "following names before search:",
+        paste(paste0("'", sp_hybrids, "'"), collapse = ", ")
+      ),
+      immediate. = TRUE,
+      call. = FALSE
+    )
   }
   # Merge multiple spaces
-  fixed7 <- gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", fixed6, perl = TRUE)
+  fixed7 <-
+    gsub("(?<=[\\s])\\s*|^\\s+|\\s+$", "", fixed6, perl = TRUE)
   return(fixed7)
 }
 
 #' @keywords internal
 simple_cap <- function(x) {
   # Split each string into words, remove unnecessary white spaces, and convert to lowercase
-  words <- sapply(strsplit(x, "\\s+"), function(words) paste(tolower(words), collapse = " "))
-
+  words <-
+    sapply(strsplit(x, "\\s+"), function(words)
+      paste(tolower(words), collapse = " "))
+  
   # Capitalize the first letter of each word
   capitalized <- sapply(strsplit(words, ""), function(word) {
     if (length(word) > 0) {
@@ -34,7 +43,7 @@ simple_cap <- function(x) {
     }
     paste(word, collapse = "")
   })
-
+  
   return(capitalized)
 }
 
@@ -48,4 +57,47 @@ find_duplicates <- function(vector) {
   return(duplicated_words)
 }
 
-
+#' Function to standtardized Departamentos names
+#' @keywords internal
+fix_departamentos <- function(x, column = 'locality') {
+  stopifnot(
+    inherits(x, 'data.frame'), # secure tidy manipulation
+    column  %in% colnames(x), # name of the column exists and
+    length(column) == 1, # is only one column and
+    is_character(column) # is type character
+  )
+  # x <- distribution
+  # column = 'locality'
+  
+  modx <- x %>% select(id, !!sym(column)) %>%
+    nest_by(id) %>%
+    mutate(data = map(data, ~ .x %>%
+                        str_split('\\|', simplify = T) %>%
+                        str_squish)) %>%
+    unnest(data) %>% ungroup() %>%
+    mutate(
+      data = case_when(
+        data %in% c("Guanía", "Guainia", "Guinía",
+                    "Guaínia", "Guania") ~ "Guainía",
+        data %in% c("Antoquia", "Anitoquia") ~ "Antioquia",
+        data == "San Andrés" ~ "San Andrés y Providencia",
+        data == "Guaviaré" ~ "Guaviare",
+        data == "Caqueta" ~ "Caquetá",
+        data == "César" ~ " Cesar",
+        data == "Quindio" ~ "Quindío",
+        data == "Cordoba" ~ "Córdoba",
+        data == "Víchada" ~ "Vichada",
+        data %in% c("", "Antioquia (?)", "Nariño (?)") ~ NA,
+        .default = as.character(data)
+      )
+    ) %>%
+    nest_by(id) %>%
+    mutate(localityCorrected = map_chr(data, ~ paste(.x, collapse = ' | ')) %>% 
+             unname()) %>% 
+    unnest(localityCorrected) %>% ungroup()
+  
+  out <- left_join(x, modx[,c('id', 'localityCorrected')], by = "id")
+  stopifnot(all(out$id  %in% x$id))
+  
+  return(out)
+}
