86b988cfea399df3bc349a6a339a0971e863babe
crcruzr <cristian.alexander.cruz.rodriguez@umontreal.ca>
2024-06-24 16:12:05 -0400

    CC - I have added the option to evaluate points in the Ocean. In addition, the user can include the vector file to do both continental and oceanic validation.

--- Coordinate_Validator/mamm_coords_validator.r
+++ Coordinate_Validator/mamm_coords_validator.r
@@ -1,8 +1,6 @@
-#testcoords<- read.csv("Coordinate_Validator/test_data_coordiantes.csv", header = T)
-#library(mammalcol)
-#library(terra)
-
 #' Validate mammal species distribution data based on geographic coordinates.
+#' Elaborated by Cristian A. Cruz Rodríguez
+#' Last upload: June 2024
 #'
 #' This function validates species distribution data provided in a data frame
 #' against known mammal species lists and geographic coordinates. It outputs
@@ -10,103 +8,230 @@
 #'
 #' @param df A data frame containing species distribution data with columns 'species',
 #'        'decimalLongitude', and 'decimalLatitude'.
+#' @param sp_names Name of the column containing species names (Genus + Specific Epithet).
 #' @param taxon A data frame with distribution information, including 'scientificName' and 'distribution'.
-#' @param colmap A spatial object (e.g., raster or vector) representing the geographic area to validate against.
+#'              The scientificName must be in binomial form, and the distribution should contain names separated by |.
+#'              By default, the function uses the checklist available at https://www.gbif.org/dataset/e8b9ed9b-f715-4eac-ae24-772fbf40d7ae.
+#' @param colmap A spatial object in vector format representing the geographic area to validate against.
+#'               By default, the function uses the Colombia Administrative Boundaries.
+#' @param lon Name of the column containing longitude values in df. Default is 'decimalLongitude'.
+#' @param lat Name of the column containing latitude values in df. Default is 'decimalLatitude'.
+#' @param adm_names Name of the column in colmap representing administrative boundaries. Default is 'NAME_1'.
+#' @param ocenamap A spatial object representing the ocean area to validate against.
+#' @param oce_adm_names Name of the column in ocenamap representing administrative boundaries for ocean areas. Default is 'ocean'.
 #' @return A data frame with validated species records and validation results.
-#'
+
 #' @details
 #' This function validates species distribution data by checking species names against a
-#' known list and verifying geographic coordinates against a specified map ('colmap').
-#' It assigns a validation result ('validation_result') where 1 means coincidence and 0 theoposive
-#' to each species record in 'df'.
+#' known list and verifying geographic coordinates against specified maps ('colmap' and 'ocenamap').
+#' It assigns a validation result ('validation_result') where 1 means coincidence and 0 means
+#' no match. Additional details are provided in the returned data frame.
 #'
-#' @references
-#' Ramírez-Chaves H E, Leuro Robles N G, Castaño Rivera A, Morales-Martínez D M, Zurc D, Suárez Castro A F, 
-#' Concha Osbahr D C, Trujillo A, Noguera Urbano E A, Pantoja Peña G E, Rodríguez-Posada M E, 
-#' González Maya J F, Pérez Torres J, Mantilla Meluk H, López Castañeda C, Velásquez Valencia A,
-#'  Zárrate Charry D (2024). Mamíferos de Colombia. Version 1.14. Sociedad Colombiana de Mastozoología.
-#'  Checklist dataset https://doi.org/10.15472/kl1whs accessed via GBIF.org on 2024-06-21.
-#' 
 #' @examples
-#' # Example usage:
-#' df <- read.csv("Coordinate_Validator/test_data_coordiantes.RData")
-#' load('data/taxon.rda')
-#' colmap <- load('data/colmap.rda')
-#' validated_data <- mamm_coords_validator(df, taxon = taxon, colmap = colmap)
+#' df <- read.csv("test_data_coordinates.csv")
+#' taxon <- read.csv("taxon.csv")
+#' colmap <- read_sf("colmap.shp")
+#' validated_data <- mamm_coords_validator(df, sp_names = "species", taxon = taxon, colmap = colmap)
 #'
 #' @export
-mamm_coords_validator <- function(df, distribution = NULL, colmap = NULL) {
-
-
-  # Validation: Check if distribution and colmap are provided
-  if (is.null(distribution))  {
-  load('data/taxon.rda')
+mamm_coords_validator <- function(df, sp_names, taxon = NULL, colmap = NULL, lon = NULL, lat = NULL, adm_names = NULL, ocenamap = NULL, oce_adm_names = NULL) {
+  # Initialize function
+  
+  # Validate input and set defaults if necessary
+  df <- as.data.frame(df)
+  oriNames <- names(df)
+  
+  # Check if species column name is provided and follows binomial structure
+  if (missing(sp_names)) {
+    stop("You must specify the name of the column containing species names (sp_names) using binomial structure (Genus + specifiepithet).")
+  }
+  
+  # Validation: Check if taxon, colmap, and column names are provided
+  if (is.null(taxon))  {
+    taxon <- mammalcol::taxon
+  }
+  
+  if (is.null(adm_names)) {
+    adm_names = 'NAME_1'
   }
   
   if (is.null(colmap)) {
-load('data/colmap.rda')
-  }
-
-
-
-  # Extract unique species names from the data frame
-  sppnms <- unique(df$species)
-  
-  # Validation of species
-  vlid_spp <- search_mammalcol(sppnms, max_distance = 0.0)
-  vlid_spp_nm <- unique(vlid_spp$name_submitted)
-  
-  # Display number of species found and validated
-  if (length(vlid_spp_nm) == 0) {
-    cat("There aren't valid species in the data set. Please review the species names before using this function")
+    colmap <- mammalcol::colmap
+    colmap[[adm_names]] <- tolower(colmap[[adm_names]])
   } else {
-    cat(length(sppnms), "species found in the matrix and ", nrow(vlid_spp), "is/are valid \n")
+    colmap <- sf::st_as_sf(colmap)
+    colmap[[adm_names]] <- tolower(colmap[[adm_names]])
   }
-  cat("...\n")
   
-  # Species not validated
-  notValispp <- df[!df$species %in% vlid_spp_nm,]
-  notValispp$validation_result <- NA
+  # Set default column names for longitude and latitude if not provided
+  if (is.null(lon) & is.null(lat))  {
+    lon = 'decimalLongitude'
+    lat = 'decimalLatitude'
+  } 
   
-  # Initialize a placeholder for the final result
+  # Set default ocean map and administrative boundary name for ocean if not provided
+  if (is.null(ocenamap)) {
+    ocenamap <-  sf::st_as_sf(Colombian_sea)
+    #ocenamap <- Colombian_sea
+  }  else {
+    ocenamap <- sf::st_as_sf(ocenamap)
+  }
+  
+  if (is.null(oce_adm_names)) {
+    ocenamap[[adm_names]] <- 'ocean'
+  } else {
+    ocenamap[[adm_names]] <- oce_adm_names
+  }
+
+  ## Start the data process
+  
+  df$IDVal <- paste0('M', 1:nrow(df))
+  
+  # Extract unique species names from the data frame
+  sppnms <- unique(df[[sp_names]])
+  
+  # Validate species names against known species list
+  vlid_spp <- search_mammalcol(sppnms, max_distance = 0.0)
+  
+  # Display summary of species validation
+  if (length(vlid_spp$name_submitted) == 0) {
+    cat("There aren't valid species in the dataset. Please review the species names before using this function.")
+  } else {
+    cat(length(sppnms), "species found in the matrix and ", nrow(vlid_spp), "is/are valid.\n")
+  }
+  
+  # Separate species into validated and non-validated
+  notValispp <- df[!df[[sp_names]] %in% unique(vlid_spp$name_submitted), ]
+  notValispp$validation_result <- 4
+  notValispp$dup.areas.val <- NA
+  
+  Valispp <- df[df[[sp_names]] %in% unique(vlid_spp$name_submitted), ]
+  
+  # Initialize placeholder for final validated results
   finalVal <- NA
   
   # Loop through each valid species
   for (i in 1:nrow(vlid_spp)) {
-    spp.i <- df[df$species %in% vlid_spp$name_submitted[i], ]
+    spp.i <- Valispp[Valispp[[sp_names]] %in% vlid_spp$name_submitted[i], ]
     
-    # Extract geographic coordinates
-    vect.spp.i <-  terra::vect(spp.i, geom = c('decimalLongitude', 'decimalLatitude'), crs = "+proj=longlat +datum=WGS84")
-    vect.spp.i.t <- terra::extract(terra::vect(colmap), vect.spp.i)
-    spp.i$validDpto <- vect.spp.i.t$NAME_1
+    # Extract and intersect species distribution with administrative boundaries
+    vect.spp.i <- terra::vect(spp.i, geom = c(lon, lat), crs = "+proj=longlat +datum=WGS84")
+    vect.spp.i.t <- terra::intersect(terra::vect(colmap), vect.spp.i)
+   
+    # extract coordinates to the file
+    if (nrow(vect.spp.i.t) > 0) {
+      vect.spp.i.t2 <- as.data.frame(vect.spp.i.t, geom = 'XY')
+      colnames(vect.spp.i.t2)[colnames(vect.spp.i.t2) == c("x")]<- lon
+      colnames(vect.spp.i.t2)[colnames(vect.spp.i.t2) == c("y")]  <- lat
+      spp.i.f <- vect.spp.i.t2[,names(spp.i)]
+      spp.i.f$validDpto<- vect.spp.i.t2[[adm_names]]
+    } else {
+      spp.i.f <- NULL
+    } 
     
-    # Append the subset to the final result
-    finalVal <- rbind(finalVal, spp.i)
+    
+    # Handle cases where records are not fully evaluated
+    if (nrow(vect.spp.i) != nrow(vect.spp.i.t)) {
+      vect.spp.i.novali <- vect.spp.i[!(vect.spp.i$IDVal %in% vect.spp.i.t$IDVal), ]
+      vect.spp.i.novali2 <- terra::intersect(terra::vect(ocenamap), vect.spp.i.novali)
+      
+      if (nrow(vect.spp.i.novali2) == 0) {
+        vect.spp.i.novali <- as.data.frame(vect.spp.i.novali, geom = 'XY')
+        colnames(vect.spp.i.novali)[colnames(vect.spp.i.novali) == c("x")] <- lon
+        colnames(vect.spp.i.novali)[colnames(vect.spp.i.novali) == c("y")]  <- lat
+        vect.spp.i.novali.f <- vect.spp.i.novali[, names(spp.i)]
+        vect.spp.i.novali.f$validDpto <- 'Other'
+        spp.i.f <- rbind(spp.i.f, vect.spp.i.novali.f)
+      } else {
+        vect.spp.i.novali <- as.data.frame(vect.spp.i.novali2, geom = 'XY')
+        colnames(vect.spp.i.novali)[colnames(vect.spp.i.novali) == c("x")] <- lon
+        colnames(vect.spp.i.novali)[colnames(vect.spp.i.novali) == c("y")]  <- lat
+        vect.spp.i.novali.f <- vect.spp.i.novali[, names(spp.i)]
+        vect.spp.i.novali.f$validDpto <- 'Ocean'
+        spp.i.f <- rbind(spp.i.f, vect.spp.i.novali.f)
+      }
+    }
+    
+    # Check for duplicate records and assign appropriate validation
+    if (any(duplicated(spp.i.f$IDVal))) {
+      dupspp <- spp.i.f[duplicated(spp.i.f$IDVal), ]
+      spp.i.f$dup.areas.val <- ifelse(spp.i.f$IDVal %in% dupspp$IDVal, 1, 0)
+    } else {
+      spp.i.f$dup.areas.val <- 0
+    }
+    
+    # Append validated species subset to final results
+    finalVal <- rbind(finalVal, spp.i.f)
   }
-  finalVal <- finalVal[-1,]
   
-  # Split distribution information
+  finalVal <- finalVal[-1, ]
+  
+  # Additional validation based on species distribution
   distribution_list <- strsplit(taxon$distribution, "\\|")
-  deptos <- as.data.frame(cbind(Depto = unique(colmap$NAME_1)))
+  finaleva <- NA
   
-  # Loop through each valid species again
   for (j in 1:nrow(vlid_spp)) {
     sp_id.j <- which(taxon$scientificName == vlid_spp$name_submitted[j])
-    unos <- trimws(distribution_list[[sp_id.j]])
+    unos <- tolower(trimws(distribution_list[[sp_id.j]]))
+    validdepto.i <- finalVal[finalVal[[sp_names]] %in% vlid_spp$name_submitted[j], ]
     
-    # Check if department matches known distribution
-    finalVal$validation_result <- ifelse(finalVal$validDpto %in% unos, 1, 0)
-    finalVal <- subset(finalVal, select = -c(validDpto))
+    evaluate_text <- function(text) {
+      if (text %in% unos) {
+        return(1)
+      } else if (text == 'Other') {
+        return(3)
+      } else if (text == 'Ocean') {
+        return(2)
+      } else {
+        return(0)
+      }
+    }
     
+    # Apply the evaluation function to assign 'validation_result'
+    validdepto.i$validation_result <- sapply(validdepto.i$validDpto, evaluate_text)
+    validdepto.i <- subset(validdepto.i, select = -c(validDpto))
+    finaleva <- rbind(finaleva, validdepto.i)
   }
   
-  finalVal <- rbind(finalVal, notValispp)
+  finaleva <- finaleva[-1, ]
   
-  # Print the counts
-  cat("Number of records with correct State:", length(which(finalVal$validation_result == 1)), "\n")
-  cat("Number of records with different State:", length(which(finalVal$validation_result == 0)), "\n \n")
-   if (is.null(distribution))  {
-  cat("All of the validation were development using the publication 'Ramirez-Chaves et. al, 2024' \n")
+  # Additional checks for duplicated records
+  t.dupl <- NA
+  tn.dupl <- NA
+  
+  if (any(finaleva$dup.areas.val %in% 1)) {
+    t.dupl <- finaleva[finaleva$dup.areas.val == 1, ]
+    tn.dupl <- finaleva[finaleva$dup.areas.val == 0, ]
+    c.dup <- unique(t.dupl$IDVal)
+    
+    for (h in 1:length(c.dup)) {
+      t.dupl.h <- t.dupl[t.dupl$IDVal %in% c.dup[h], ]
+      if (all(t.dupl.h$validation_result == 1)) {
+        tn.dupl <- rbind(tn.dupl, t.dupl.h[1, ])
+      } else if (all(t.dupl.h$validation_result == 0)) {
+        tn.dupl <- rbind(tn.dupl, t.dupl.h[1, ])
+      } else {
+        t.dupl.h$validation_result <- 3
+        tn.dupl <- rbind(tn.dupl, t.dupl.h[1, ])
+      }
+    }
+  } else {
+    tn.dupl <- finaleva
   }
-  return(finalVal)
+  
+  # Combine final validated and non-validated species data
+  finalVal <- rbind(tn.dupl, notValispp)
+  
+  # Return final validated data frame
+  finalValT <- finalVal[, c(oriNames, 'validation_result')]
+  
+  cat('Validation Finished.\n')
+  cat('A total of', nrow(df), 'records were evaluated. The evaluation results are recorded in the "validation_result" column as follows:\n')
+  cat('- 0 = Valid species but records not registered within the analyzed boundaries.\n')
+  cat('- 1 = Valid species and coordinates according to official publications.\n')
+  cat('- 2 = Valid species and coordinates are registered in the ocean.\n')
+  cat('- 3 = Valid species and coordinates are within the limits of the ocean administrative boundaries. We recommend reviewing the location manually.\n')
+  cat('- 4 = Not valid species and not validated.\n')
+  
+  return(finalValT)
 }
